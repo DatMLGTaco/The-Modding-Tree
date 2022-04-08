031949ee36c764fed9ca6c78ad460db329cb0427
DatMLGTaco <66452144+DatMLGTaco@users.noreply.github.com>
2022-04-08 17:47:53 -0500

    Update tree.js

--- js/tree.js
+++ js/tree.js
@@ -14,19 +14,6 @@ addNode("blank", {
     layerShown: "ghost",
 }, 
 )
-addNode("g", {
-    symbol: "TH",
-    branches: [["m"]],
-    color: '#6d3678',
-    layerShown: true,
-    canClick() {return player.points.gte(10)},
-    tooltip: "Thanos your points",
-    tooltipLocked: "Thanos your points",
-    onClick() {player.points = player.points.div(2)
-    console.log(this.layer)}
-
-}, 
-)
 
 addLayer("tree-tab", {
     tabFormat: [["tree", function() {return (layoutInfo.treeLayout ? layoutInfo.treeLayout : TREE_LAYERS)}]],
