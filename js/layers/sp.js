12f567f6008cded6508851779ff6e078fc97f00c
DatMLGTaco <DatMLGTaco@gmail.com>
2022-04-25 21:22:23 -0500

    layer not unlocked fix

--- js/layers/sp.js
+++ js/layers/sp.js
@@ -123,7 +123,9 @@ addLayer("sp", {
     }
     },
     
-    image: "https://media1.giphy.com/media/moztbdp3Y93zlq7sIH/giphy.gif?cid=790b7611dc90382df7b886eee3fda23e71749404e9b6e703&rid=giphy.gif&ct=g",
+    image(){
+        if (!player.sp.unlocked) return ""
+        return "https://media1.giphy.com/media/moztbdp3Y93zlq7sIH/giphy.gif?cid=790b7611dc90382df7b886eee3fda23e71749404e9b6e703&rid=giphy.gif&ct=g" },
     style() {
     return {"background-image" : "url('https://media3.giphy.com/media/UZLkKp1DiyP9LRmCcB/giphy.gif?cid=790b76119561afe82af5799af3760b150438217d11c28c33&rid=giphy.gif&ct=g')"
     }},
